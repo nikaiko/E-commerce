a6193210ce72720c882978436572a1127f89dec5
nikaiko <nikolaevkolya3105@gmail.com>
2022-08-24 01:58:03 +0900

    header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,17 +2,27 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
-import Logo from "./Logo.svg";
-
+import Bag from "./components/Bag";
+import Logo from "./components/Logo";
+import User from "./components/User";
 import "./Header.scss";
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
     <header className="header">
-      <Link to="/">
-        <img src={Logo} alt="Lalasia logo" />
+      <Link to="/" className="header__logo">
+        <Logo />
       </Link>
-      Header
+      <div className="header__nav">
+        <Link to="/product">Product</Link>
+        <Link to="/setvices">Setvices</Link>
+        <Link to="/article">Article</Link>
+        <Link to="/about">About Us</Link>
+      </div>
+      <div className="header__icons">
+        <Bag />
+        <User />
+      </div>
     </header>
   );
 };
--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 import Header from "@components/Header";
 import { Outlet } from "react-router-dom";
+import "./MainLayout.scss";
 
-const MainLayout = () => {
+const MainLayout: React.FC = () => {
   return (
     <div className="wrapper">
       <Header />
