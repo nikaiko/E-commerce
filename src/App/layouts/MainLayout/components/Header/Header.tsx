1632b880ed487be2ea67ff60762d17885676b130
nikaiko <nikolaevkolya3105@gmail.com>
2022-09-13 01:24:26 +0900

    fix comments of revie

--- src/App/layouts/MainLayout/components/Header/Header.tsx
+++ src/App/layouts/MainLayout/components/Header/Header.tsx
@@ -6,43 +6,43 @@ import profileIcon from "@assets/images/profile-icon.svg";
 import titleSvg from "@assets/images/title.svg";
 import routes from "@configs/routes";
 import cn from "classnames";
-import { Link, NavLink } from "react-router-dom";
+import * as Router from "react-router-dom";
 
 import MenuButton from "./components/MenuButton";
 import s from "./Header.module.scss";
 
 const Header: React.FC = () => {
-  const setActive = ({ isActive }: { isActive: boolean }) =>
-    `${s.header__link} ${isActive && s.header__link_active}`;
-  // isActive ? s.header__link_active : s.header__link;
+  const styleLink = React.useCallback(({ isActive }: { isActive: boolean }) => {
+    return cn(s.header__link, isActive && s.header__link_active);
+  }, []);
 
   return (
     <header className={s.header}>
-      <Link to={routes.main.mask} className={s.header__logoTitle}>
+      <Router.Link to={routes.main.mask} className={s.header__logoTitle}>
         <img src={logoSvg} alt="logo-svg" className={s.header__logo} />
         <img src={titleSvg} alt="title" className={s.header__title} />
-      </Link>
+      </Router.Link>
       <nav className={s.header__nav}>
-        <NavLink to={routes.products.mask} className={setActive}>
+        <Router.NavLink to={routes.products.mask} className={styleLink}>
           Product
-        </NavLink>
-        <NavLink to={routes.services.mask} className={setActive}>
+        </Router.NavLink>
+        <Router.NavLink to={routes.services.mask} className={styleLink}>
           Services
-        </NavLink>
-        <NavLink to={routes.article.mask} className={setActive}>
+        </Router.NavLink>
+        <Router.NavLink to={routes.article.mask} className={styleLink}>
           Article
-        </NavLink>
-        <NavLink to={routes.about.mask} className={setActive}>
+        </Router.NavLink>
+        <Router.NavLink to={routes.about.mask} className={styleLink}>
           About Us
-        </NavLink>
+        </Router.NavLink>
       </nav>
       <div className={s.header__icons}>
-        <Link to={routes.profile.mask}>
+        <Router.Link to={routes.profile.mask}>
           <img src={profileIcon} alt="profile-icon" />
-        </Link>
-        <Link to={routes.basket.mask}>
+        </Router.Link>
+        <Router.Link to={routes.basket.mask}>
           <img src={basketIcon} alt="basket-icon" />
-        </Link>
+        </Router.Link>
       </div>
       <MenuButton />
     </header>
